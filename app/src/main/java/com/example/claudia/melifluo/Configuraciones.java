11f5b555859eb5fb42b1dcc1c28f48c9b215ff5e
Claudia <chamg7@hotmail.com>
2018-06-16 18:30:30 -0500

    Perfil.Mod_Perfil.Crear + vinculaciones

--- app/src/main/java/com/example/claudia/melifluo/Configuraciones.java
+++ app/src/main/java/com/example/claudia/melifluo/Configuraciones.java
@@ -1,13 +1,28 @@
 package com.example.claudia.melifluo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Configuraciones extends AppCompatActivity {
+
+    public Button mod_usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuraciones);
+
+        mod_usuario = (Button)findViewById(R.id.mod_us);
+
+        mod_usuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent per = new Intent(Configuraciones.this, Perfil_Modificar.class);
+                startActivity(per);
+            }
+        });
     }
 }
