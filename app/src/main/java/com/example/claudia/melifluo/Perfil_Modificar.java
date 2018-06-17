97dd2406310a11a64cb882cc88a48eb7ef1c817e
Claudia <chamg7@hotmail.com>
2018-06-16 20:12:54 -0500

    Fecha

--- app/src/main/java/com/example/claudia/melifluo/Perfil_Modificar.java
+++ app/src/main/java/com/example/claudia/melifluo/Perfil_Modificar.java
@@ -1,6 +1,7 @@
 package com.example.claudia.melifluo;
 
 import android.app.DatePickerDialog;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,21 +9,35 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
 
+import java.text.DateFormat;
 import java.util.Calendar;
 
-public class Perfil_Modificar extends AppCompatActivity implements View.OnClickListener{
+public class Perfil_Modificar extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
+
+    public Button cumple;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_perfil__modificar);
+
+        cumple = (Button)findViewById(R.id.nac);
+        cumple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(), "date picker");
+            }
+        });
 
     }
 
     @Override
-    public void onClick(View v) {
-
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DATE, dayOfMonth);
+        String currentDateString = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
     }
 }
-
-
