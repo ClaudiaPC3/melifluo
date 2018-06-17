21c187b80bb071da3023aa93251dd59fa3b7e754
Claudia <chamg7@hotmail.com>
2018-06-16 19:02:59 -0500

    Perfil.Mod_Perfil.Crear botones

--- app/src/main/java/com/example/claudia/melifluo/Perfil_Crear.java
+++ app/src/main/java/com/example/claudia/melifluo/Perfil_Crear.java
@@ -1,13 +1,20 @@
 package com.example.claudia.melifluo;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.DatePicker;
 
-public class Perfil_Crear extends AppCompatActivity {
+public class Perfil_Crear extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil__crear);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
     }
 }
