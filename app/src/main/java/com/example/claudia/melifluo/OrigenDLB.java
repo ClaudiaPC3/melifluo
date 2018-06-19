d9db505dfbf2dee2667f89db029c61e899be487a
i-saw-sparks <andresmolinaceti@gmail.com>
2018-06-18 20:56:02 -0500

    Intento de hacer Busqueda dinamnica. ERROR

---
+++ app/src/main/java/com/example/claudia/melifluo/OrigenDLB.java
@@ -0,0 +1,52 @@
+package com.example.claudia.melifluo;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+
+public class OrigenDLB {
+    public Context context;
+    public ArrayList<BusqPojo>showAll(){
+        ArrayList<BusqPojo> resultado=new ArrayList<BusqPojo>();
+
+        final BusqPojo unica = new BusqPojo();
+        String url  = "http://192.168.0.25/Programas/M_Libro_Bus.php?Valor=0";
+        JsonArrayRequest peticion = new JsonArrayRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+                            unica.BPttl=(""+response.getString(0));
+                            unica.BPimg=(""+response.getString(1));
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                ,new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }
+        );
+        RequestQueue x= Volley. newRequestQueue(context);
+        x.add(peticion);
+        return resultado;
+    }
+}
