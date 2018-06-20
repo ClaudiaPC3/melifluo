9557fe469fd6e99cd4ea9e60c4d558bff386fbcd
i-saw-sparks <andresmolinaceti@gmail.com>
2018-06-20 17:53:57 -0500

    Mi biblioteca al 100, falta hascer la consulta en la segunda actividad que abre.

--- app/src/main/java/com/example/claudia/melifluo/MiLibro.java
+++ app/src/main/java/com/example/claudia/melifluo/MiLibro.java
@@ -1,13 +1,23 @@
 package com.example.claudia.melifluo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MiLibro extends AppCompatActivity {
 
+    public String clave;
+    public TextView titulo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mi_libro);
+        Intent recper = getIntent();
+        clave = recper.getStringExtra("id");
+        titulo = (TextView)findViewById(R.id.titulo);
+        titulo.setText(clave);
     }
 }
